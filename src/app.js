95ad12d3d8f51c7781d2189fa565003ba471f3e3
zoe14c <zulfakaryusuf14@gmail.com>
2024-06-04 21:24:33 +0700

    coba lagi

--- src/app.js
+++ src/app.js
@@ -1,44 +1,19 @@
-document.addEventListener('alpine:init', () => {
-	Alpine.data('products', () => ({
+document.addEventListener("alpine:init", () => {
+	Alpine.data("products", () => ({
 		items: [
-			{ id: 1, name: 'Robusta Brazil1', img: '01.jpg', price: 15000 },
-			{ id: 2, name: 'Robusta Brazil2', img: '02.jpg', price: 20000 },
-			{ id: 3, name: 'Robusta Brazil3', img: '03.jpg', price: 25000 },
-			{ id: 4, name: 'Robusta Brazil4', img: '04.jpg', price: 30000 },
-			{ id: 5, name: 'Robusta Brazil5', img: '05.jpg', price: 35000 },
-			{ id: 6, name: 'Robusta Brazil6', img: '06.jpg', price: 40000 },
-			{ id: 7, name: 'Robusta Brazil7', img: '07.jpg', price: 45000 },
-			{ id: 8, name: 'Robusta Brazil8', img: '08.jpg', price: 50000 },
-			{ id: 9, name: 'Robusta Brazil9', img: '09.jpg', price: 55000 },
-			{ id: 10, name: 'Robusta Brazil10', img: '10.jpg', price: 15000 },
-			{ id: 11, name: 'Robusta Brazil11', img: '11.jpg', price: 15000 },
-			{ id: 12, name: 'Robusta Brazil12', img: '12.jpg', price: 20000 },
-			{ id: 13, name: 'Robusta Brazil13', img: '13.jpg', price: 25000 },
-			{ id: 14, name: 'Robusta Brazil14', img: '14.jpg', price: 30000 },
-			{ id: 15, name: 'Robusta Brazil15', img: '15.jpg', price: 35000 },
-			{ id: 16, name: 'Robusta Brazil16', img: '16.jpg', price: 40000 },
-			{ id: 17, name: 'Robusta Brazil17', img: '17.jpg', price: 45000 },
-			{ id: 18, name: 'Robusta Brazil18', img: '18.jpg', price: 50000 },
-			{ id: 19, name: 'Robusta Brazil19', img: '19.jpg', price: 55000 },
-			{ id: 20, name: 'Robusta Brazil20', img: '20.jpg', price: 15000 },
-			{ id: 21, name: 'Robusta Brazil21', img: '21.jpg', price: 15000 },
-			{ id: 22, name: 'Robusta Brazil22', img: '22.jpg', price: 20000 },
-			{ id: 23, name: 'Robusta Brazil23', img: '23.jpg', price: 25000 },
-			{ id: 24, name: 'Robusta Brazil24', img: '24.jpg', price: 30000 },
-			{ id: 25, name: 'Robusta Brazil25', img: '25.jpg', price: 35000 },
-			{ id: 26, name: 'Robusta Brazil26', img: '26.jpg', price: 40000 },
-			{ id: 27, name: 'Robusta Brazil27', img: '27.jpg', price: 45000 },
-			{ id: 28, name: 'Robusta Brazil28', img: '28.jpg', price: 50000 },
-			{ id: 29, name: 'Robusta Brazil29', img: '29.jpg', price: 55000 },
-			{ id: 30, name: 'Robusta Brazil30', img: '30.jpg', price: 55000 },
+			{ id: 1, name: "Robusta Brazil", img: "01.jpg", price: 15000 },
+			{ id: 2, name: "Primo Passo", img: "02.jpg", price: 20000 },
+			{ id: 3, name: "Arabica Blend", img: "03.jpg", price: 25000 },
+			{ id: 4, name: "Aceh Mayo", img: "04.jpg", price: 30000 },
+			{ id: 5, name: "Cyanide Cofe", img: "05.jpg", price: 35000 },
 		],
 	}));
 
-	Alpine.store('cart', {
+	Alpine.store("cart", {
 		items: [],
 		quantity: 0,
 		total: 0,
-      
+
 		add(newItem) {
 			// cek isi keranjang.
 			const cartItem = this.items.find((item) => item.id === newItem.id);
@@ -52,7 +27,7 @@ document.addEventListener('alpine:init', () => {
 				this.items = this.items.map((item) => {
 					// jika barang beda
 					if (item.id !== newItem.id) {
-						return items;
+						return item;
 					} else {
 						// jika sudah ada, tambah quantity dan totalnya
 						item.quantity++;
@@ -83,45 +58,69 @@ document.addEventListener('alpine:init', () => {
 				});
 			} else if (cartItem.quantity === 1) {
 				// Jika barangnya sisa 1
-				this.items = this.items.filter(item => item.id !== id);
+				this.items = this.items.filter((item) => item.id !== id);
 				this.quantity--;
 				this.total -= cart.item.price;
-			};
+			}
 		},
 	});
 });
 
 // Form Validation
-const checkoutButton = document.querySelector("checkout-button");
+const checkoutButton = document.querySelector("#checkoutForm");
 checkoutButton.disabled = true;
 
-const form = document.querySelector("#checkout-form");
-form.addEventListener('keyup', function () {
+const form = document.querySelector("checkout-button");
+form.addEventListener("keyup", function () {
 	for (let i = 0; i < form.elements.length; i++) {
 		if (form.elements[i].value.length !== 0) {
-         checkoutButton.classList.remove('disabled');
-         checkoutButton.classList.add('disabled');
-      } else {
-         return false;
-      }
+			checkoutButton.classList.remove("disabled");
+			checkoutButton.classList.add("disabled");
+		} else {
+			return false;
+		}
 	}
 	checkoutButton.disabled = false;
-	checkoutButton.classList.remove('disabled');
+	checkoutButton.classList.remove("disabled");
 });
 
-// Kirim data ketika tombol checkout disumbit.
-checkoutButton.addEventListener('click', function (e) {
+// Kirim data ketika tombol checkout diklik.
+checkoutButton.addEventListener("click", async function (e) {
 	e.preventDefault();
 	const formData = new FormData(form);
 	const data = new URLSearchParams(formData);
-	const objData = Object.fromEntries(data).items;
-   console.log(objData);
+	const objData = Object.fromEntries(data);
+	// const message = formatMessage(objData);
+	// window.open("https://wa.me/62817377322?text=" + encodeURIComponent(message));
+
+	// Minta token transaksi menggunakan ajax /fetch
+	try {
+		const response = await fetch("php/placeOrder.php", {
+			method: "POST",
+			body: data,
+		});
+		const token = await response.text();
+		// Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token
+		window.snap.pay(token);
+	} catch (err) {
+		console.log(err.message);
+	}
 });
 
+// Pesan WA
+const formatMessage = (obj) => {
+	return `Data Customer
+	Nama: ${obj.name}
+	Email: ${obj.email}
+	No HP: ${obj.phone}
 
+	Data Pesanan 
+	${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)} ) \n`)}
 
-
-
+	TOTAL: ${rupiah(obj.total)}
+	
+	Terima kasih.`;
+};
 // JavaScript Demo: Intl.NumberFormat
 // const number = 1234567.89;
 // console.log(new Intl.NumberFormat('id-ID').format(number));
@@ -130,9 +129,9 @@ checkoutButton.addEventListener('click', function (e) {
 // console.log(new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'EUR' }).format(number));
 
 const rupiah = (number) => {
-	return new Intl.NumberFormat('id-ID', {
-		style: 'currency',
-		currency: 'IDR',
+	return new Intl.NumberFormat("id-ID", {
+		style: "currency",
+		currency: "IDR",
 		minimumFractionDigits: 0,
 	}).format(number);
 };
