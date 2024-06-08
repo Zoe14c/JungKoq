37cba2f4738ab7aeecb07a4df24d6bf5e29e9657
zoe14c <zulfakaryusuf14@gmail.com>
2024-06-08 13:52:14 +0700

    3x

--- php/placeOrdes.php
+++ php/placeOrdes.php
@@ -22,16 +22,15 @@ require_once dirname(__FILE__) . '/midtrans-php-master/Midtrans.php';
 $params = array(
    'transaction_details' => array(
       'order_id' => rand(),
-      'gross_amount' => $_POST('total'),
+      'gross_amount' => $_POST['total'],
    ),
    'item_details' => json_decode($_POST['items'], true),
    'customer_details' => array(
-      'first_name' => $_POST('name'),
-      'email' => $_POST('email'),
-      'phone' => $_POST('phone'),
+      'first_name' => $_POST['name'],
+      'email' => $_POST['email'],
+      'phone' => $_POST['phone'],
    ),
 );
 
 $snapToken = \Midtrans\Snap::getSnapToken($params);
 echo $snapToken;
-?>
